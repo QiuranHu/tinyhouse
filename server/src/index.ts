3a4c0c8898d2c3b1e0e926ad676fb5505b7cd0b0
Qiuran Hu <qiuranh@gmail.com>
2020-10-27 22:23:22 +0800

    Creating a GraphQL Schema with the GraphQL JS Library

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,11 @@
 import express from "express";
-import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
+import { schema } from "./graphql";
 const app = express();
 const port = 9000;
 
-app.use(express.json());
-
-app.get("/listings", (_req, res) => {
-  return res.send(listings);
-});
-
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send("failed to delete listing");
-});
+const server = new ApolloServer({ schema }); // Create an ApolloServer instance.
+server.applyMiddleware({ app, path: "/api" }); // Connect ApolloServer with Express. GraphQL API is on /api path.
 
 app.listen(port);
 
