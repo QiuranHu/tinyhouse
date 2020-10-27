7db6f0ded5d4b97e3c693b365142d5c5d66b8475
Qiuran Hu <qiuranh@gmail.com>
2020-10-28 07:37:59 +0800

    Using the GraphQL Schema Language

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { schema } from "./graphql";
+import { typeDefs, resolvers } from "./graphql";
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer({ schema }); // Create an ApolloServer instance.
+const server = new ApolloServer({ typeDefs, resolvers }); // Create an ApolloServer instance.
 server.applyMiddleware({ app, path: "/api" }); // Connect ApolloServer with Express. GraphQL API is on /api path.
 
 app.listen(port);
