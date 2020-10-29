aafb2667e324135b64f03dc32dcde9665ceed78d
Qiuran Hu <qiuranh@gmail.com>
2020-10-29 22:34:02 +0800

    Processing Data from MongoDB

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,21 +1,35 @@
-import { listings } from "../listings";
-import {IResolvers} from "apollo-server-express";
+import { IResolvers } from "apollo-server-express";
+import { Database, Listing } from "../lib/types";
+import { ObjectId } from "mongodb";
 
-export const resolvers:IResolvers = {
+export const resolvers: IResolvers = {
   Query: {
-    listings: () => {
-      return listings;
+    listings: async (
+      _root: undefined,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing[]> => {
+      return await db.listings.find({}).toArray();
     },
   },
   Mutation: {
-    deleteListing: (_root: undefined, { id }: {id: string}) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
-      }
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ): Promise<Listing> => {
+      const deleteRes = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id),
+      });
 
-      throw new Error("failed to deleted listing");
+      if (!deleteRes.value) {
+        throw new Error("failed to delete listing");
+      }
+      return deleteRes.value;
     },
+  },
+  Listing: {
+    id: (listing: Listing): string => listing._id.toString(),
   },
 };
