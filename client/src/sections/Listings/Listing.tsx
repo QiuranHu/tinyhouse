4ef0d050e278b3329119c9c8a2b19fbda7616518
Qiuran Hu <qiuranh@gmail.com>
2020-10-30 23:58:00 +0800

    Querying Listings

--- client/src/sections/Listings/Listing.tsx
+++ client/src/sections/Listings/Listing.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { server } from "../../lib/api";
 
 interface Props {
   title: string;
 }
 
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      rating
+    }
+  }
+`;
 export const Listings = ({ title }: Props) => {
-  return <h2>{title}</h2>;
+  const fetchListings = async () => {
+    const listings = await server.fetch({ query: LISTINGS });
+    console.log(listings);
+  };
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query Listings!</button>
+    </div>
+  );
 };
