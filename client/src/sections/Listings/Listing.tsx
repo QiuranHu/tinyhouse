aeff627c957cf2d0e484136d80c391201cd98c5e
Qiuran Hu <qiuranh@gmail.com>
2020-11-05 22:38:11 +0800

    Custom useQuery Hook

--- client/src/sections/Listings/Listing.tsx
+++ client/src/sections/Listings/Listing.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { server } from "../../lib/api";
+import React from "react";
+import { server, useQuery } from "../../lib/api";
 import {
   DeleteListingData,
   DeleteListingVariables,
-  Listing,
   ListingsData,
 } from "./types";
 
@@ -35,14 +34,7 @@ const DELETE_LISTING = `
 `;
 
 export const Listings = ({ title }: Props) => {
-  const [listings, setListings] = useState<Listing[] | null>(null);
-  useEffect(() => {
-    fetchListings();
-  }, []);
-  const fetchListings = async () => {
-    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
-    setListings(data.listings);
-  };
+  const { data } = useQuery<ListingsData>(LISTINGS);
   const deleteListing = async (id: string) => {
     await server.fetch<DeleteListingData, DeleteListingVariables>({
       query: DELETE_LISTING,
@@ -50,9 +42,9 @@ export const Listings = ({ title }: Props) => {
         id,
       },
     });
-    fetchListings();
   };
 
+  const listings = data ? data.listings : null;
   const listingsList = listings ? (
     <ul>
       {listings.map((listing) => (
@@ -67,7 +59,6 @@ export const Listings = ({ title }: Props) => {
     <div>
       <h2>{title}</h2>
       {listingsList}
-      <button onClick={fetchListings}>Query Listings!</button>
     </div>
   );
 };
