0355a049d1ed1dcd398a8b2918be03903acb9b13
Qiuran Hu <qiuranh@gmail.com>
2020-11-08 19:07:00 +0800

    React Router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/react-hooks";
 import reportWebVitals from "./reportWebVitals";
-import { Listings } from "./sections";
+import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
 import "./styles/index.css";
 
 const client = new ApolloClient({
@@ -11,9 +12,24 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  )
+}
+
 render(
   <ApolloProvider client={client}>
-    <Listings title="Tinyhouse Listings" />
+    <App />
   </ApolloProvider>,
   document.getElementById("root")
 );
