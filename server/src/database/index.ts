8581eccf2dbbb07a660ce96ed5319cb8d14adb89
Qiuran Hu <qiuranh@gmail.com>
2020-11-15 18:05:19 +0800

    Database Document Structure

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,13 +1,18 @@
 // Our goal is to export a funciton when run will make the connection with the database.
 import { MongoClient } from "mongodb";
-import { Database } from "../lib/types";
+import { Database, User, Listing, Booking } from "../lib/types";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/test?retryWrites=true&w=majority`;
 
 export const connectDatabase = async (): Promise<Database> => {
-  const client = await MongoClient.connect(url, { useNewUrlParser: true,useUnifiedTopology: true });
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   const db = client.db("main"); // Access the main database.
   return {
-    listings: db.collection("test_listings"), // Get the test_listings collection under the main database.
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"), // Get the test_listings collection under the main database.
   };
 };
