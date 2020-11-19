5af11aaf5a0b0cf30c5deb9b2619373d87e1f204
Qiuran Hu <qiuran.hu@sap.com>
2020-11-19 15:35:45 +0800

    Building the UI for Login

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/react-hooks";
 import reportWebVitals from "./reportWebVitals";
-import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
+import { Home, Host, Listing, Listings, NotFound, User, Login } from "./sections";
 import "./styles/index.css";
+import { Layout } from 'antd';
 
 const client = new ApolloClient({
   uri: "/api",
@@ -15,14 +16,18 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/host" component={Host} />
-        <Route exact path="/listing/:id" component={Listing} />
-        <Route exact path="/listings/:location?" component={Listings} />
-        <Route exact path="/user/:id" component={User} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout id='app'>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route exact path="/user/:id" component={User} />
+          <Route exact path="/login" component={Login} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+
     </Router>
   )
 }
